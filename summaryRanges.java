0c93341738c42f3e133215c7b5e67d1d484a7587
RaniaSalama <r7ibrahi@uwaterloo.ca>
2017-10-10 16:33:58 -0400

    Create summaryRanges.java

---
+++ summaryRanges.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int i = 0;
+        List<String> ranges = new ArrayList<String>();
+        int n = nums.length;
+        while (i < n) {
+            int firstNum = nums[i];
+            while((i+1 < n) && (nums[i+1]-nums[i] == 1)) {
+                i++;
+            }
+            if(nums[i] == firstNum){
+                ranges.add(nums[i]+"");
+            } else {
+                ranges.add(firstNum+"->"+nums[i]);
+            }
+            i++;
+        }
+        return ranges;
+    }
+}
