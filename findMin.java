d795ec3960c70b396c7462d89a72f8ba304228f5
RaniaSalama <r7ibrahi@uwaterloo.ca>
2017-10-10 17:08:13 -0400

    Create findMin.java

---
+++ findMin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+        while(i+1 < n && nums[i+1] > nums[i]) {
+            i++;
+        }
+        if(i+1 < n)
+            return Math.min(nums[i+1], nums[0]);
+        else
+            return nums[0];
+    }
+}
