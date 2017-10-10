4bf532b70815c4cd95d789d76bcb233e0e1add55
RaniaSalama <r7ibrahi@uwaterloo.ca>
2017-10-10 16:51:02 -0400

    Create combinationSum3.java

---
+++ combinationSum3.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public List<List<Integer>> solve(int k, int n, int start) {
+        List<List<Integer>> allSolutions = new ArrayList<List<Integer>>();
+        if(k == 1 && n >= start && n <= 9){ // Base case.
+            List<Integer> solution = new ArrayList<Integer>();
+            if(!solution.contains(n)){
+                solution.add(n);
+                allSolutions.add(solution);
+            }
+            return allSolutions;
+        }
+        if(k < 1) { // empty!
+            return allSolutions;
+        }
+        for(int x = start; x <= 9; x++) {
+            List<List<Integer>>  solutions = solve(k-1, n-x, x+1);
+            for(List<Integer> solution: solutions) {
+                solution.add(x);
+                allSolutions.add(solution);
+            }
+        }
+        return allSolutions;
+    }
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        return solve(k, n, 1);
+    }
+}
+
