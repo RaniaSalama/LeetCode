7da9e9e9e2ac25b4064f1d826442bfda4d4e6297
RaniaSalama <r7ibrahi@uwaterloo.ca>
2017-10-10 17:00:56 -0400

    Create minSubArrayLen.java

---
+++ minSubArrayLen.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        int[] sum = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            sum[i] = sum[i-1] + nums[i-1];
+        }
+        for(int w = 1; w <= n; w++){
+            for(int i = 0; i <= n - w; i++) {
+                if(sum[i+w] - sum[i]>= s) {
+                    return w;
+                }
+            }
+        }
+        return 0;
+    }
+}
+
+
